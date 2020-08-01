a4dc4b0cf3ee43475cc2e331edd283114dcd521e
AleksanderTech <aleksanderroztrop@gmail.com>
2020-08-01 11:17:19 +0200

    Domain address fix

--- src/main/resources/js/url-provider.js
+++ src/main/resources/js/url-provider.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = 'http://alexecute:7000';
-export const SERVER_API_URL = 'http://alexecute:7000/api';
+export const SERVER_URL = 'http://alexecute.com:7000';
+export const SERVER_API_URL = 'http://alexecute.com:7000/api';
 export const THOUGHT_API_URL = SERVER_API_URL + '/thought';
 export const TAG_API_URL = SERVER_API_URL + '/tag';
 
